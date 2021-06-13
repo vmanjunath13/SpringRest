fb95fb5eb99646ab20a74f5242b61181b3a53b99
vmanjunath13 <vmanjunath@hawk.iit.edu>
2021-06-12 22:23:32 -0500

    Application Initializer Added

---
+++ src/main/java/ennate/io/AppInitializer.java
@@ -0,0 +1,20 @@
+package ennate.io;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{Application.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/api/*"};
+    }
+}
